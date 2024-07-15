d834160a7cccb04d43fc2ebdf3eb626225c5b172
azrinsaedi <azrinsaedi@gmail.com>
2024-07-16 00:56:17 +0800

    set to 0.0.0.0

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     https: true,
-    host: 'loafycat.com',
+    host: '0.0.0.0',
     port: 443,
     strictPort: true,
     proxy: {
